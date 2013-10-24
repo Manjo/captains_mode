31a88ed5f2cccd3f839513521b219ea581fd5fe4
Travis Hall <trvs.hll@gmail.com>
2013-10-23 23:57:41 -0700

    Changed SASS structure. Made the CSS more modular.

--- app/helpers/champions_helper.rb
+++ app/helpers/champions_helper.rb
@@ -4,8 +4,18 @@ module ChampionsHelper
 		"/images/headshots/#{champion_tag(c)}.jpg"
 	end
 
+	def champion_headshot_image(c, options={})
+		return "" if c.nil?
+		image_tag(champion_headshot_path(c), options)
+	end
+
 	def champion_portrait_path(c)
 		"/images/portraits/#{champion_tag(c)}.jpg"
+	end
+
+	def champion_portrait_image(c, options={})
+		return "" if c.nil?
+		image_tag(champion_portrait_path(c), options)
 	end
 
 	def champion_tag(c)
--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,35 @@ class Team < ActiveRecord::Base
 		@captain_id == id
 	end
 
+	def picks(min=0)
+		p = super
+
+		if min > 0
+			p = p.to_a
+			(min - p.size).times do
+				p << ChampionPick.new
+			end
+		end
+		p
+	end
+
+
+	# If no arguments are supplied, acts normally by returning the
+	# CollectionProxy. If a min is supplied, it will convert to an array
+	# and try to fill the array to the minimum size
+	def bans(min=0)
+		b = super
+
+		if min > 0
+			b = b.to_a
+			(min - b.size).times do
+				b << ChampionBan.new
+			end
+		end
+
+		b
+	end
+
 	def ban(champion)
 		ban = ChampionBan.new(champion: champion)
 		bans << ban
